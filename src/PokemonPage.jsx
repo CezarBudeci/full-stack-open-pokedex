2439849f57b2404230c35664f1ca0188736024ac
Cezar Budeci <cezarbudeci@gmail.com>
2023-10-27 21:16:03 +0300

    Fixed lint errors

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -33,8 +33,6 @@ const PokemonPage = ({ previous, next }) => {
     const hiddenAbility = pokemon.abilities.find(
         ability => ability.is_hidden === true
     );
-
-    // eslint-disable-line no-console
     console.log('hiddenAbility=', hiddenAbility);
     return (
         <>
